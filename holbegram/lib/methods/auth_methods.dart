6505d00573a6d961f3337b8354ac008668b133c4
SDINAHET <stephane.dinahet@gmail.com>
2025-12-17 04:04:47 +0100

    task6

---
+++ holbegram/lib/methods/auth_methods.dart
@@ -0,0 +1,80 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/user.dart';
+
+class AuthMethode {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  /// LOGIN
+  Future<String> login({
+    required String email,
+    required String password,
+  }) async {
+    if (email.isEmpty || password.isEmpty) {
+      return 'Please fill all the fields';
+    }
+
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return 'success';
+    } on FirebaseAuthException catch (e) {
+      return e.message ?? 'Login failed';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  /// SIGN UP
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    Uint8List? file,
+  }) async {
+    if (email.isEmpty || password.isEmpty || username.isEmpty) {
+      return 'Please fill all the fields';
+    }
+
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      User user = userCredential.user!;
+
+      // TODO: upload `file` to Cloudinary (http) and get photoUrl
+      final String photoUrl = '';
+
+      final Users users = Users(
+        uid: user.uid,
+        email: email,
+        username: username,
+        bio: '',
+        photoUrl: photoUrl,
+        followers: [],
+        following: [],
+        posts: [],
+        saved: [],
+        searchKey: username.isNotEmpty ? username[0].toLowerCase() : '',
+      );
+
+      await _firestore.collection("users").doc(user.uid).set(users.toJson());
+
+      return 'success';
+    } on FirebaseAuthException catch (e) {
+      return e.message ?? 'Sign up failed';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+}
