31c82241e12edd9cb871da3a7896503cc8e23e36
SDINAHET <stephane.dinahet@gmail.com>
2025-12-20 23:58:20 +0100

    Clean Holbegram

--- holbegram/lib/screens/home.dart
+++ holbegram/lib/screens/home.dart
@@ -1,20 +1,3 @@
-// import 'package:flutter/material.dart';
-// import '../widgets/bottom_nav.dart';
-
-// class Home extends StatefulWidget {
-//   const Home({super.key});
-
-//   @override
-//   State<Home> createState() => _HomeState();
-// }
-
-// class _HomeState extends State<Home> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const BottomNav();
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/user_provider.dart';
